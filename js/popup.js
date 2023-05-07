3ecb0aa1c35ec61137c17ed78bab18f3481cffc6
Louay Mbarki <louay.mbarki23@gmail.com>
2023-05-07 23:13:37 +0200

    Update Errors

--- js/popup.js
+++ js/popup.js
@@ -6,32 +6,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
   startTranscriptionBtn.addEventListener('click', () => {
     chrome.runtime.sendMessage({ action: 'startTranscription' }, response => {
-      if (response.success) {
+      if (response && response.success) {
         startTranscriptionBtn.disabled = true;
         stopTranscriptionBtn.disabled = false;
+      } else {
+        console.log(chrome.runtime.lastError);
       }
     });
   });
 
   stopTranscriptionBtn.addEventListener('click', () => {
     chrome.runtime.sendMessage({ action: 'stopTranscription' }, response => {
-      if (response.success) {
+      if (response && response.success) {
         startTranscriptionBtn.disabled = false;
         stopTranscriptionBtn.disabled = true;
+      } else {
+        console.log(chrome.runtime.lastError);
       }
     });
   });
 
   viewFullTranscriptBtn.addEventListener('click', () => {
-    chrome.runtime.sendMessage({ action: 'viewFullTranscript' });
+    chrome.runtime.sendMessage({ action: 'viewFullTranscript' }, response => {
+      if (!response || !response.success) {
+        console.log(chrome.runtime.lastError);
+      }
+    });
   });
 
   shareContentBtn.addEventListener('click', () => {
-    chrome.runtime.sendMessage({ action: 'shareContent' });
+    chrome.runtime.sendMessage({ action: 'shareContent' }, response => {
+      if (!response || !response.success) {
+        console.log(chrome.runtime.lastError);
+      }
+    });
   });
 
   chrome.runtime.sendMessage({ action: 'getTranscriptionStatus' }, response => {
-    if (response.transcribing) {
+    if (response && response.transcribing) {
       startTranscriptionBtn.disabled = true;
       stopTranscriptionBtn.disabled = false;
     } else {
